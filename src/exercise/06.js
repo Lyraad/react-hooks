6c2b7d13220f30e4f5a0dc20a437eb1bf9c1d821
Daryl Hewett <dazza1308+github@gmail.com>
2021-11-14 22:40:21 +0000

    ex credit 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
+
 import {
   PokemonForm,
   fetchPokemon,
@@ -14,31 +11,39 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
+  const [status, setStatus] = React.useState('idle')
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if (!pokemonName) return
-    setPokemon(null)
+    setStatus('request')
     fetchPokemon(pokemonName)
-      .then(pokemonData => setPokemon(pokemonData))
+      .then(pokemonData => {
+        setPokemon(pokemonData)
+        setStatus('success')
+      })
       .catch(e => {
         setError(e)
+        setStatus('error')
       })
   }, [pokemonName])
-  if (error) {
+
+  if (status === 'idle') {
+    return 'Submit a pokemon'
+  } else if (status === 'error') {
     return (
       <div role="alert">
         There was an error:{' '}
         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
       </div>
     )
-  } else if (!pokemonName) {
-    return 'Submit a pokemon'
-  } else if (!pokemon) {
+  } else if (status === 'request') {
     return <PokemonInfoFallback name={pokemonName} />
-  } else {
+  } else if (status === 'success') {
     return <PokemonDataView pokemon={pokemon} />
+  } else {
+    throw new Error(`Unexpected state ${status}`)
   }
 }
 
